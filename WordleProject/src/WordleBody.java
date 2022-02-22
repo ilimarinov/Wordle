ae152f0f67c9be8428c04395e48dd2a0a4e52b18
Iliyan <iliyanalmarinov@gmail.com>
2022-02-22 16:17:23 +0200

    Language versions added

--- WordleProject/src/WordleBody.java
+++ WordleProject/src/WordleBody.java
@@ -1,39 +1,184 @@
-public class WordleBody {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
-    private static final int MAX_GUESSES = 5;
-    private static final int WORD_LENGTH = 5;
-    private static int currentGuess = 1;
+public abstract class WordleBody {
 
-    private String word;
-    private String guessedWord;
+    private static final char IN_POSITION_GUESS = '*';
+    private static final char NOT_IN_POSITION_GUESS = '+';
+    private static final char NOT_EXISTING_LETTER = '-';
 
+    private static final int WORDLE_CHARACTERS = 5;
+    private static final int ATTEMPTS = 5;
 
-    public WordleBody(String word, String guessesWord) {
-        setGuessedWord(guessesWord);
-        setWord(word);
+    private static List<String> words = new ArrayList<>();
 
-    }
+    private static String wordle;
+    private static String guessWord = "";
+    private static Character[] currentGuess = new Character[WORDLE_CHARACTERS];
+    private static char currentLetterGuessed;
 
-    public String getWord() {
-        return word;
-    }
+    private static Set<Character> charactersInWordle = new HashSet<>();
+    private static SortedSet<Character> notInPositionLetters = new TreeSet<>();
+    private static SortedSet<Character> inPositionLetters = new TreeSet<>();
+    private static SortedSet<Character> notUsedLetters = new TreeSet<>();
 
-    public void setWord(String word) {
-        if (word.length() != WORD_LENGTH) {
-            throw new IllegalArgumentException(String.format("The word's length must be %d characters long", WORD_LENGTH));
+    private static List<String> duplicateWords = new ArrayList<>();
+
+    private static Map<Character, Integer> guessWordLettersCount = new HashMap<>();
+    private static Map<Character, Integer> wordleLettersCount = new HashMap<>();
+
+    protected void Solver() {
+        setWord();
+
+        notUsedLetters = AlphabetGenerator();
+
+        for (int g = 0; g < WORDLE_CHARACTERS; g++) {
+            charactersInWordle.add(wordle.charAt(g));
         }
-        this.word = word;
-    }
+        for (int i = 0; i < ATTEMPTS; i++) {
 
-    public String getGuessedWord() {
-        return guessedWord;
-    }
+            setGuessedWord();
 
-    public void setGuessedWord(String guessedWord) {
-        if (guessedWord.length() != WORD_LENGTH) {
-            throw new IllegalArgumentException(String.format("The word's length must be %d characters long", WORD_LENGTH));
+            duplicateWords.add(guessWord);
+
+            if (guessWord.equals(wordle)) {
+                System.out.print(messageCorrectWord());
+                return;
+            }
+
+            inPositionLetters.clear();
+            notInPositionLetters.clear();
+
+            wordleResetCounter();
+            guessWordResetCounter();
+
+            for (int j = 0; j < WORDLE_CHARACTERS; j++) {
+                currentLetterGuessed = guessWord.charAt(j);
+                notUsedLetters.remove(currentLetterGuessed);
+
+                if (charactersInWordle.contains(currentLetterGuessed) && wordle.charAt(j) == currentLetterGuessed) {
+                    currentGuess[j] = IN_POSITION_GUESS;
+                    inPositionLetters.add(currentLetterGuessed);
+                    guessWordLettersCount.put(currentLetterGuessed, guessWordLettersCount.get(currentLetterGuessed) - 1);
+                } else if (charactersInWordle.contains(currentLetterGuessed)
+                        &&
+                        guessWordLettersCount.get(currentLetterGuessed) <= (wordleLettersCount.get(currentLetterGuessed))) {
+                    currentGuess[j] = NOT_IN_POSITION_GUESS;
+                    guessWordLettersCount.put(currentLetterGuessed, guessWordLettersCount.get(currentLetterGuessed) - 1);
+                    notInPositionLetters.add(currentLetterGuessed);
+                } else {
+                    currentGuess[j] = NOT_EXISTING_LETTER;
+                }
+
+                System.out.print("[" + currentGuess[j] + "]");
+
+                if (charactersInWordle.contains(currentLetterGuessed)) {
+                    wordleLettersCount.put(currentLetterGuessed, wordleLettersCount.get(currentLetterGuessed) + 1);
+                }
+            }
+            System.out.printf("%n");
+            System.out.printf(messageInPositionLetters() + inPositionLetters + "%n");
+            System.out.printf(messageNotInPositionLetters() + notInPositionLetters + "%n");
+            System.out.printf(messageNotUsedLetters() + notUsedLetters + "%n");
         }
-        this.guessedWord = guessedWord;
+        System.out.printf("%n" + messageWordNotGuessed() + "%s%n", wordle);
     }
+
+    protected String getWord() {
+        return wordle;
+    }
+
+    private void setWord() {
+        BufferedReader reader = null;
+
+        try {
+            String currentLine;
+            reader = new BufferedReader(new FileReader(pathToWordleList()));
+            while ((currentLine = reader.readLine()) != null) {
+                words.add(currentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (reader != null) reader.close();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+                ex.printStackTrace();
+            }
+        }
+
+        Random rand = new Random(System.currentTimeMillis());
+        wordle = words.get(rand.nextInt(words.size()));
+    }
+
+    private String getGuessedWord() {
+        return guessWord;
+    }
+
+    private void setGuessedWord() {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            do {
+                do {
+                    if (guessWord.length() != WORDLE_CHARACTERS) {
+                        System.out.printf(messageStart() + "%n", WORDLE_CHARACTERS);
+                    }
+                    guessWord = scanner.nextLine();
+                } while (guessWord.length() != WORDLE_CHARACTERS);
+                if (!(words.contains(guessWord))) {
+                    System.out.printf(messageNotExistingWord() + "%n");
+                }
+            } while (!(words.contains(guessWord)));
+            if ((duplicateWords.contains(guessWord))) {
+                System.out.printf(messageDuplicateWord() + "%n");
+            }
+        } while ((duplicateWords.contains(guessWord)));
+    }
+
+    private Map<Character, Integer> LettersCounter(String word) {
+        Map<Character, Integer> lettersCounter = new HashMap<>();
+        for (char x : word.toCharArray()) {
+            if (lettersCounter.containsKey(x)) {
+                int count = lettersCounter.get(x);
+                lettersCounter.put(x, count + 1);
+            } else {
+                lettersCounter.put(x, 1);
+            }
+        }
+        return lettersCounter;
+    }
+
+    private void wordleResetCounter() {
+        wordleLettersCount.clear();
+        wordleLettersCount = LettersCounter(wordle);
+    }
+
+    private void guessWordResetCounter() {
+        guessWordLettersCount.clear();
+        guessWordLettersCount = LettersCounter(guessWord);
+    }
+
+    protected abstract SortedSet<Character> AlphabetGenerator();
+
+    protected abstract String messageStart();
+
+    protected abstract String messageDuplicateWord();
+
+    protected abstract String messageCorrectWord();
+
+    protected abstract String messageWordNotGuessed();
+
+    protected abstract String messageNotExistingWord();
+
+    protected abstract String messageInPositionLetters();
+
+    protected abstract String messageNotInPositionLetters();
+
+    protected abstract String messageNotUsedLetters();
+
+    protected abstract String pathToWordleList();
 }
-
---
+++ WordleProject/src/WordleEnglish.java
@@ -0,0 +1,74 @@
+
+import java.util.*;
+
+public class WordleEnglish extends WordleBody {
+
+    public static final String START_MESSAGE = "Write a word. Must be %d characters.";
+    public static final String DUPLICATE_WORD_MESSAGE = "The word has already been used! Write a new word.";
+    public static final String CORRECT_WORD_MESSAGE = "The word is correct!";
+    public static final String WORD_NOT_GUESSED_MESSAGE = "The word is: ";
+    public static final String NOT_EXISTING_WORD_MESSAGE = "The word must be part of the wordle's list.";
+    public static final String IN_POSITION_LETTERS_MESSAGE = "In position: ";
+    public static final String NOT_IN_POSITION_LETTERS_MESSAGE = "Not in position letters: ";
+    public static final String NOT_USED_LETTERS_MESSAGE = "Not Used letters so far: ";
+
+    public static final String PATH = "/Users/iliyanmarinov/Wordle/wordleEnglish.txt";
+
+    public WordleEnglish() {
+        super.Solver();
+    }
+
+    public SortedSet<Character> AlphabetGenerator() {
+        SortedSet<Character> Alphabet = new TreeSet<>();
+        for (char a = 'a'; a <= 'z'; ++a) {
+            Alphabet.add(a);
+        }
+        return Alphabet;
+    }
+
+    @Override
+    protected String messageStart() {
+        return START_MESSAGE;
+    }
+
+    @Override
+    protected String messageDuplicateWord() {
+        return DUPLICATE_WORD_MESSAGE;
+    }
+
+    @Override
+    protected String messageCorrectWord() {
+        return CORRECT_WORD_MESSAGE;
+    }
+
+    @Override
+    protected String messageWordNotGuessed() {
+        return WORD_NOT_GUESSED_MESSAGE;
+    }
+
+    @Override
+    protected String messageNotExistingWord() {
+        return NOT_EXISTING_WORD_MESSAGE;
+    }
+
+    @Override
+    protected String messageInPositionLetters() {
+        return IN_POSITION_LETTERS_MESSAGE;
+    }
+
+    @Override
+    protected String messageNotInPositionLetters() {
+        return NOT_IN_POSITION_LETTERS_MESSAGE;
+    }
+
+    @Override
+    protected String messageNotUsedLetters() {
+        return NOT_USED_LETTERS_MESSAGE;
+    }
+
+    @Override
+    protected String pathToWordleList() {
+        return PATH;
+    }
+}
+
