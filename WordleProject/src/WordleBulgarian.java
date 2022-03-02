d7b246e77107fe56d278cd8f74b8be86fd899b42
Iliyan <iliyanalmarinov@gmail.com>
2022-03-02 21:22:34 +0200

    switched alphabet to correct classes

--- WordleProject/src/WordleBulgarian.java
+++ WordleProject/src/WordleBulgarian.java
@@ -19,8 +19,8 @@ public class WordleBulgarian extends WordleBody {
 
     public SortedSet<Character> AlphabetGenerator() {
         SortedSet<Character> Alphabet = new TreeSet<>();
-        Collections.addAll(Alphabet, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-                'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+        Collections.addAll(Alphabet, 'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й',
+                'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ','ь','ю', 'я');
         return Alphabet;
     }
 
--- WordleProject/src/WordleEnglish.java
+++ WordleProject/src/WordleEnglish.java
@@ -20,8 +20,8 @@ public class WordleEnglish extends WordleBody {
 
     public SortedSet<Character> AlphabetGenerator() {
         SortedSet<Character> Alphabet = new TreeSet<>();
-        Collections.addAll(Alphabet, 'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й',
-                'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ','ь','ю', 'я');
+        Collections.addAll(Alphabet, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
         return Alphabet;
     }
 
